b73fd23cf190d88686a275b13f5e1a9a627b842b
Jinpluff <melladoromain@gmail.com>
2016-04-11 21:00:59 +0200

    Ajout du programme de la fonction main

---
+++ main prgm.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+"""
+Éditeur de Spyder
+
+Ceci est un script temporaire.
+"""
+
+
+def main():
+    liste_role = []
+    liste_nom = []
+    liste_morts = []
+    nombre_loups = parametrage() #-> int la fonction parametrage doit demander le nombre de joueurs et parametrer les cartes en jeu.
+    if joueur_de_flute in liste_role:
+        compteur_joueur_de_flute=0
+    if cupidon in liste_role:
+        amoureux=[]
+    if ancien in liste_role:
+        ancien1=1
+    if corbeau in liste_role:
+        maudit=[]
+    if enfant_sauvage in liste_role:
+        enfant_sauvage1=0
+    #servante_devouee ?
+    #les_3_freres ?
+    #les_2_soeurs ?
+    #montreur-d'ours difficile a mettre en pace
+    #comedien ?
+    if chevalier_a_l_epee_rouillee in liste_role:
+        chavalier=0
+    if juge_begue in liste_role:
+        juge=0
+    #abominable_sectaire ?
+    if infect_pere_des_loups in liste_role:
+        infect=0
+    #chien_loup ?
+    
+    morts_tour_1=tour1(liste_role,liste_nom) #-> str| la fontion tour1 doit executer les fonctions de personnages intervenent a la premiere nuit, modifier les listes en fonction des défunts, et ressortir un message disant qui sont les morts.
+    
+    
+    
+    
+#list.sort permet de trier une liste, soir par ordre alphabetique, soit par ordre croissant
