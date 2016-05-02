45762d5a92981ece212e2433acf974d1c8821fa8
Jinpluff <melladoromain@gmail.com>
2016-05-02 12:27:37 +0200

    update main, ajout nuit_tour_1

--- main prgm.py
+++ main prgm.py
@@ -1,43 +1,29 @@
-# -*- coding: utf-8 -*-
 """
-Éditeur de Spyder
-
-Ceci est un script temporaire.
+Programme loup garou : fonction main
 """
-
+"""
+Variables utilisées :
+liste_role : liste des personnages utilisés
+liste_nom : liste des joueurs ajustés avec leur role
+liste_mort : liste éphémère qui répertorie les morts pour les indiquer a la sorciere
+nombre_loups : indique le nombre de loups. Est utilisé pour verifier a chaque debut de tour si la partie est finie.
+"""
 
 def main():
     liste_role = []
     liste_nom = []
-    liste_morts = []
-    nombre_loups = parametrage() #-> int la fonction parametrage doit demander le nombre de joueurs et parametrer les cartes en jeu.
-    if joueur_de_flute in liste_role:
-        compteur_joueur_de_flute=0
+    liste_mort = []
+    nombre_loups = parametrage_role() #-> int la fonction parametrage_role doit demander le nombre de joueurs et parametrer les cartes en jeu.
+    parametrage_nom(liste_role) #-> rien la fonction parametrage_nom affiche une boite où il faut entrer les noms des joueurs a coté des nom des cartes
     if cupidon in liste_role:
         amoureux=[]
-    if ancien in liste_role:
-        ancien1=1
-    if corbeau in liste_role:
-        maudit=[]
-    if enfant_sauvage in liste_role:
-        enfant_sauvage1=0
-    #servante_devouee ?
-    #les_3_freres ?
-    #les_2_soeurs ?
-    #montreur-d'ours difficile a mettre en pace
-    #comedien ?
-    if chevalier_a_l_epee_rouillee in liste_role:
-        chavalier=0
-    if juge_begue in liste_role:
-        juge=0
-    #abominable_sectaire ?
-    if infect_pere_des_loups in liste_role:
-        infect=0
-    #chien_loup ?
-    
-    morts_nuit_tour_1=nuit_tour1(liste_role,liste_nom) #-> str| la fontion tour1 doit executer les fonctions de personnages intervenent a la premiere nuit, modifier les listes en fonction des défunts, et ressortir un message disant qui sont les morts.
+    if sorciere in liste_role:
+        potion_vie=1
+        potion_mort=1
+    nuit_tour1(liste_role,liste_nom): #-> str| la fontion tour1 doit executer les fonctions de personnages intervenent a la premiere nuit, modifier les listes en fonction des défunts, et ressortir un message disant qui sont les morts.
     
     
+
     
     
 #list.sort permet de trier une liste, soir par ordre alphabetique, soit par ordre croissant
---
+++ nuit_tour_1.py
@@ -0,0 +1,7 @@
+"""
+Programme loup garou : fonction nuit_tour_1
+"""
+
+def nuit_tour1(liste_role,liste_nom):
+    if voleur in liste_role:
+        
